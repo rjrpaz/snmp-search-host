59e07cd28f69bfb7a3039e1140449c86adaa2e98
Roberto J. R. Paz <rjrpaz@gmail.com>
2022-04-02 00:25:30 -0300

    Use a more generic name for file

--- Datos.pm
+++ Datos.pm
@@ -1,4 +1,4 @@
-package TresSesentaNet;
+package Datos;
 
 use strict;
 require Exporter;
---
+++ alter_table_aps.sql
@@ -0,0 +1,3 @@
+use tecnica;
+ALTER TABLE aps ADD snmpro VARCHAR (20);
+ALTER TABLE aps ADD snmprw VARCHAR (20);
